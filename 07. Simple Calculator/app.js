074917e6ca645312f3d3404402721ed08af420ec
Emil Mitev <82056900+EmilMitev741@users.noreply.github.com>
2023-01-13 17:11:29 +0200

    Add files via upload

---
+++ 07. Simple Calculator/app.js
@@ -0,0 +1,27 @@
+function calculator() {
+    let num1 = null;
+    let num2 = null;
+    let result = null;
+
+    return {
+        init,
+        add,
+        subtract
+    }
+
+    function init(n1, n2, resSelect) {
+        num1 = document.querySelector(n1);
+        num2 = document.querySelector(n2);
+        result = document.querySelector(resSelect);
+    }
+
+    function add() {
+        result.value = Number(num1.value) + Number(num2.value);
+    }
+    function subtract() {
+        result.value = Number(num1.value) - Number(num2.value);
+    }
+
+
+
+}
